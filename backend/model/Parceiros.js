41843a605b4ef2562dc037195e2aa1622a7e1368
Marcelle Pernet <mevpernet@gmail.com>
2022-10-13 16:59:33 -0300

    unique IDs ok

--- backend/model/Parceiros.js
+++ backend/model/Parceiros.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose"
 const { Schema, model } = mongoose
 
 const parceirosSchema = new Schema({
-    id: Number,
-    tradingName: String
+    id: { type: Number, required: true, min: 1, index: true, unique: true },
+    tradingName: String,
 })
 
 const Parceiros = model('parceiros', parceirosSchema)
