a4d6e2d7452086a6653d42c9ee5389b5a9b5dfe9
Marcelle Pernet <mevpernet@gmail.com>
2022-10-18 15:40:32 -0300

    funcionalidade Buscar Parceiro ok

--- backend/controllers/buscar.js
+++ backend/controllers/buscar.js
@@ -10,20 +10,31 @@ const buscarParceiro = asyncHandler(async (req, res) => {
     const results = await Parceiros.find().select('id address.coordinates')
     const list = results.map(({address, id}) => {
         return [distance(point(address.coordinates), userAddress, {units: 'kilometers'}), id]
-    })
-    const closest = list.sort((a, b) => a[0] - b[0]).slice(0, 3).filter(d => d[0] < 20)
+    }) 
+    // lista todas as distâncias entre o usuário e as lojas parceiras
 
-    if (closest) {
+    const maxDistanceAllowed = 20
+    const closest = list
+        .sort((a, b) => a[0] - b[0])
+        .slice(0, 3)
+        .filter(d => d[0] < maxDistanceAllowed) 
+    // ordena e seleciona as 3 mais próximas, dentro do limite de 20km
 
-        let parceiroFinal = await Parceiros.find({id: closest[0][1]}).select('-_id')
-        let coverage = parceiroFinal[0].coverageArea
-        if (booleanPointInPolygon(userAddress, multiPolygon(coverage.coordinates))) {
-            res.send(parceiroFinal)
+    if (!closest.length) { res.send('Não há nenhum parceiro próximo à sua localização.') }
+    // se houver pelo menos um parceiro próximo, verifica se sua área de cobertura atende o usuário
+    else {
+
+        let isCovered = false;
+        for (let i = 0; i < closest.length; i++) {
+            let parceiroFinal = await Parceiros.find({id: closest[i][1]}).select('-_id')
+            let coverage = parceiroFinal[0].coverageArea
+            isCovered = booleanPointInPolygon(userAddress, multiPolygon(coverage.coordinates))
+            if (isCovered) {
+                res.send(parceiroFinal)
+                break
+            }
         }
-
-    } else {
-
-        res.send('Desculpe, não há nenhum parceiro próximo à sua localização. :(')
+        if (!isCovered) { res.send('Localização fora da área de cobertura.') }
 
     }
 })
